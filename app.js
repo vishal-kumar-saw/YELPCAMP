101cf4b44a198975b67c084eb9d776980032f4b1
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2021-06-06 12:26:54 +0530

    Finish basic styles and layouts bootstrap

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -21,7 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
     res.render('home');
