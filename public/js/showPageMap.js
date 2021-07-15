62240a740171c6030036e7ea42ee03803a87205f
Vishal Kumar Saw <vishalkrsaw19035059@gmail.com>
2021-07-15 14:56:15 +0530

    finish basic styles

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -5,6 +5,9 @@ const map = new mapboxgl.Map({
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10  // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
 .setLngLat(campground.geometry.coordinates)
 .setPopup(
